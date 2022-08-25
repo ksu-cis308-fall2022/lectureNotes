d2ca43b7d44ab650aa797ee8706f36af3b42d863
julie <julie@DESKTOP-5L3T3C6>
2022-08-25 14:28:14 -0500

    after Thursday's lab

--- lab1/hello.c
+++ lab1/hello.c
@@ -1,8 +1,16 @@
 //Hello, world goes here
 #include <stdio.h>
 
+double area(double);
+
 int main() {
     printf("Hello, world!\n");
 
+    double answer = area(3.5);
+
     return 0;
+}
+
+double area(double rad) {
+    return rad*rad*3.14;
 }
