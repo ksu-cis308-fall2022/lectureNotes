99691e38fabd7a764e3c15b9d3c6b77875e1c808
julie <julie@DESKTOP-5L3T3C6>
2022-08-30 14:09:36 -0500

    notes from Tuesday, August 30

--- lab2/week2.c
+++ lab2/week2.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
+int count_letter(char str[], char letter);
+
+//Tuesday, August 30
 int main() {
     int test[10];
 
-    //show test[100] too 
-
-    //printf("%d\n", test[10000]);
+    printf("%d\n", test[10000]);
 
     return 0;
 }
+
+int count_letter(char str[], char letter) {
+    int count = 0;
+    int pos = 0;
+    while (str[pos] != '\0') {
+        if (str[pos] == letter) {
+            count++;
+        }
+        pos++;
+    }
+
+    return count;
+}
+
