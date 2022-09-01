bc80cfe70db3cc3132e078974697c6a261966a66
julie <julie@DESKTOP-5L3T3C6>
2022-09-01 14:12:28 -0500

    after Thursday's lab, Sep 1

--- lab2/week2.c
+++ lab2/week2.c
@@ -1,26 +1,10 @@
 #include <stdio.h>
 
-int count_letter(char str[], char letter);
 
-//Tuesday, August 30
+//Thursday, September 1
 int main() {
-    int test[10];
-
-    printf("%d\n", test[10000]);
+    int nums[5];
+    printf("%d\n", nums[100000]);
 
     return 0;
 }
-
-int count_letter(char str[], char letter) {
-    int count = 0;
-    int pos = 0;
-    while (str[pos] != '\0') {
-        if (str[pos] == letter) {
-            count++;
-        }
-        pos++;
-    }
-
-    return count;
-}
-
