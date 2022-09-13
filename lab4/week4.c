648a76a8561387296ee63891ee4baca1c44cdc37
julie <julie@DESKTOP-5L3T3C6>
2022-09-13 14:05:45 -0500

    after Tues Sep 13

--- lab4/week4.c
+++ lab4/week4.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
-void swap(int, int);
+void swap(int*, int*);
 
 void printarray(int*, int);
 
 int main() {
     int x = 3;
     int y = 4;
+    int nums[] = {1,2,3,4,5,6,7,8,9,10};
 
     printf("Old values: x = %d, y = %d\n", x, y);
 
-    swap(x, y);
+    swap(&x,&y);
 
     printf("New values: x = %d, y = %d\n", x, y);
+
+    printarray(nums, 10);
 
     return 0;
 }
 
 //this won't work
-void swap(int a, int b) {
-	int temp = a;
-	a = b;
-	b = temp;
+void swap(int* a, int* b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 void printarray(int* nums, int len) {
-
+    int* tempPtr = nums;
+    while (tempPtr < nums + len) {
+        printf("%d ", *tempPtr);
+        tempPtr++;
+    }
 }
 
