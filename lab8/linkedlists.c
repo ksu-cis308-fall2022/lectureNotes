5e294cad9889d9295dfc267233b72d79764e5ed4
julie <julie@DESKTOP-5L3T3C6>
2022-10-18 14:02:27 -0500

    added Tues Oct 18

--- lab8/linkedlists.c
+++ lab8/linkedlists.c
@@ -1,37 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node {
+typedef struct node {
     int data;
     struct node* next;
-};
+} NODE;
 
-struct node *head;
+void add(int elem, NODE** headptr);
 
-void add(int elem);
+void printlist(NODE* head);
 
-void printlist(void);
-
-void releasemem(void);
+void releasemem(NODE* head);
 
 int main() {
-    head = NULL;
+    NODE* head = NULL;
 
-    //add a few numbers
-    //print the list
+    //add 1-4
+    //print it
+
+    for (int i = 1; i <= 4; i++) {
+        add(i, &head);
+    }
+
+    printlist(head);
+    releasemem(head);
 
     return 0;
 }
 
-void add(int elem) {
+void add(int elem, NODE** headptr) {
     //add new node with elem to end of head's list
+    NODE* newnode = malloc(sizeof(NODE));
+    newnode->data = elem;
+    newnode->next = NULL;
+
+    if (*headptr == NULL) {
+        *headptr = newnode;
+    }
+    else {
+        NODE* temp = *headptr;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        //temp is the last node
+        temp->next = newnode;
+    }
 }
 
-void printlist(void) {
+void printlist(NODE* head) {
     //print head's list
+    NODE* temp = head;
+    while (temp != NULL) {
+        printf("%d\n", temp->data);
+        temp = temp->next;
+    }
 }
 
-void releasemem(void) {
+void releasemem(NODE* head) {
     //release all memory in the list
-
+    NODE* temp = head;
+    while (temp != NULL) {
+        NODE* after = temp->next;
+        free(temp);
+        temp = after;
+    }
 }
--- lab8/structs.c
+++ lab8/structs.c
@@ -6,9 +6,9 @@ struct person {
     int age;
 };
 
-void printperson(struct person p);
+void printperson(struct person* p);
 
-struct person birthday(struct person p);
+void birthday(struct person* p);
 
 int main() {
     struct person p1;
@@ -16,18 +16,18 @@ int main() {
     p1.age = 20;
 
     //print p1, make p1 have a birthday, print p1 again;
-    printperson(p1);
-    p1 = birthday(p1);
-    printperson(p1);
+    printperson(&p1);
+    birthday(&p1);
+    printperson(&p1);
     return 0;
 }
 
-void printperson(struct person p) {
+void printperson(struct person* p) {
     //print p's name and age
-    printf("Name: %s, age: %d\n", p.name, p.age);
+    printf("Name: %s, age: %d\n", p->name, p->age);
 }
 
-struct person birthday(struct person p) {
+void birthday(struct person* p) {
     //make p be one year older
-    p.age++;
-    return p;
+    p->age++;
+}
