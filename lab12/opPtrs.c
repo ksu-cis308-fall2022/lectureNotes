8e48c3c11cbb2fd6956b35050da45bc1cb6345b7
julie <julie@DESKTOP-5L3T3C6>
2022-11-14 10:46:17 -0600

    before Tues Nov 15 lab

---
+++ lab12/opPtrs.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int plus(int a, int b) {
+    return a + b;
+}
+
+int minus(int a, int b) {
+    return a - b;
+}
+
+int times(int a, int b) {
+    return a * b;
+}
+
+int divide(int a, int b) {
+    return a / b;
+}
+
+//Define doOperations function. Takes three parameters:
+    //function pointer to a function that does the operation
+        //(that takes two integer arguments and returns an integer result)
+    //two integers
+//returns result of whatever operation function
+
+int main() {
+    int num1 = 3;
+    int num2 = 4;
+
+    //add and multiply num1 and num2
+    //use only the doOperation function
+
+    return 0;
+}
