1d8179abee6221379cf887f2408ae046946b070a
julie <julie@DESKTOP-5L3T3C6>
2022-11-15 14:06:13 -0600

    after Tues Nov 15

--- lab12/opPtrs.c
+++ lab12/opPtrs.c
@@ -16,17 +16,27 @@ int divide(int a, int b) {
     return a / b;
 }
 
+typedef int (*op_type)(int, int);
+
+int doOperation(op_type fn, int x, int y) {
+    return fn(x, y);
+}
+
 //Define doOperations function. Takes three parameters:
     //function pointer to a function that does the operation
         //(that takes two integer arguments and returns an integer result)
     //two integers
 //returns result of whatever operation function
 
+
+
 int main() {
     int num1 = 3;
     int num2 = 4;
 
     //add and multiply num1 and num2
+    int answer = doOperation(multiply, num1, num2);
+
     //use only the doOperation function
 
     return 0;
