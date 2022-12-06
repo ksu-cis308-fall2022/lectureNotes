891b75600a4f7aaea43946961a21efccb99560a5
julie <julie@DESKTOP-5L3T3C6>
2022-12-06 14:06:42 -0600

    after Tues Dec 6

--- lab13/test.c
+++ lab13/test.c
@@ -4,6 +4,9 @@ int main() {
     int num = 4;
     void* ptr = &num;
 
+    //how to initialize ptr to point to num?
+
+    //how to print num using ptr?
     printf("%d\n", *((int*)ptr));
 
     return 0;
