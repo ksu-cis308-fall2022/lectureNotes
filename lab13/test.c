0d7d793bb79ca96d4038d7aab24adae30034649d
julie <julie@DESKTOP-5L3T3C6>
2022-12-07 13:57:45 -0600

    before Thurs lab

--- lab13/test.c
+++ lab13/test.c
@@ -2,12 +2,17 @@
 
 int main() {
     int num = 4;
-    void* ptr = &num;
+    void* ptr;
 
     //how to initialize ptr to point to num?
 
     //how to print num using ptr?
-    printf("%d\n", *((int*)ptr));
+
+    char* str = "hello";
+    
+    //make ptr point to hello
+
+    //use ptr to print the "h"
 
     return 0;
 }
